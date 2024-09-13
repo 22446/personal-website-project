8fb477a25b7565ee8e2e78abafc154d9226fc4fa
mazin safwat <mazin.safwat34@gmail.com>
2024-09-13 16:26:13 +0300

    showMorePorject

--- js/index.js
+++ js/index.js
@@ -8,6 +8,16 @@ $(".nav-item ").on("click",function(e){
 $("a[href^='#']").on("click",function(e){
     let current=e.target.getAttribute("href")
     let offsett=$(current).offset().top;
-    $("body,html").animate({scrollTop:offsett},1500)
+    $("body,html").animate({scrollTop:offsett},1000)
    
 })
+$(".clickToShow").on('click',function(){
+    $('.showmore').toggleClass('d-none')
+    if(!$('.showmore').hasClass('d-none')){
+        $('.clickToShow').html('Show Less')
+    }else{
+        $('.clickToShow').html('Show More')
+
+    }
+})
+
